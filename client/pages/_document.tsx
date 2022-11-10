58fe1aa9bb08944f16dee4bdb2d004f76ebf1f62
alex.bortnitskyi <alexey@neonpilots.co.uk>
2022-11-10 23:41:01 +0100

    WSB06 - added Google Analytics

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,10 +1,26 @@
 import { Head, Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 import { fonts } from '@/config/constants'
 
 const Document = () => (
   <Html lang="en">
     <Head>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+        `}
+      </Script>
       {fonts.map((item: string) => (
         <link href={item} rel="stylesheet" key={item} />
       ))}
